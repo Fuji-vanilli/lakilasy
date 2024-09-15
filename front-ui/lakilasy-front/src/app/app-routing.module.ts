ebf2c8afd3ef24755a5ab1c7e90c79102c5ff752
Fuji-vanilli <ijufillivan@gmail.com>
2024-09-15 17:24:13 +0300

    update

--- front-ui/lakilasy-front/src/app/app-routing.module.ts
+++ front-ui/lakilasy-front/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { StudentComponent } from './pages/student/student.component';
 
 const routes: Routes = [
-  { path: 'admin', component: SidebarComponent }
+  { path: 'admin', component: SidebarComponent, children: [
+    { path: 'student', component: StudentComponent }
+  ] }
 ];
 
 @NgModule({
